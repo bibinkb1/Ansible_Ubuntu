b7344519fe2dc97cd86f19abd2d6ba44141d05c6
bibinkb1 <bibinkb444@gmail.com>
2024-05-07 10:34:18 +0530

    07/05/2024

--- ansible masterinstallation.py
+++ ansible masterinstallation.py
@@ -2,7 +2,7 @@ import os
 os.system("sudo apt install software-properties-common -y")
 os.system("sudo apt-add-repository ppa:ansible/ansible -y")
 os.system("sudo apt --fix-broken install")
-os.system("apt update -y")
+os.system("sudo apt update -y")
 os.system("sudo apt install ansible -y")
 #configuration steps for local remote hosts
 #step1:control node ssh-keygen 
